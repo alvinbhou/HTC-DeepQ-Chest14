a587659a7333b6ffa32a80287d31224f87c9dc52
Cryolite <cryolite@ntu.im>
2018-01-15 03:20:43 +0800

    classifier working

--- copyData.py
+++ copyData.py
@@ -15,12 +15,21 @@ LABELS = [
 #     os.makedirs(os.path.join('data', 'test', d)) 
 
 from random import shuffle
-with open('data/pickles/labels_train.pkl', 'rb') as f:
+
+with open('data/pickles/labels_valid.pkl', 'rb') as f:
     traindata = pickle.load(f)
 
 for k, v in traindata.items(): 
     print(k)
     src = 'data/images/' + k
-    dest = os.path.join('data', 'train', LABELS[v], k)
+    dest = os.path.join('data', 'validation', LABELS[v], k)
     shutil.copyfile(src, dest)
+# with open('data/pickles/labels_train.pkl', 'rb') as f:
+#     traindata = pickle.load(f)
+
+# for k, v in traindata.items(): 
+#     print(k)
+#     src = 'data/images/' + k
+#     dest = os.path.join('data', 'train', LABELS[v], k)
+#     shutil.copyfile(src, dest)
         
